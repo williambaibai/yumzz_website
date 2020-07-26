624d4ef7826de1318e17f37a36b70bff07761413
williambaibai <williambaibai@gmail.com>
2020-07-26 14:37:35 -0700

    Add routing to register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
+  { path: '', component: HomepageComponent},
   { path:'register', component: RegisterPageComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
