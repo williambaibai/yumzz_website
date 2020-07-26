64e8a9f06ec5d55af47dc03ca771d5685344e382
williambaibai <williambaibai@gmail.com>
2020-07-26 14:03:41 -0700

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'register', component: RegisterPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
